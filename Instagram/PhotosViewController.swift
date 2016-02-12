eae5ab151ed6481e10c577f84aee8a707be16e4d
myulz <myles1@umbc.edu>
2016-02-11 21:04:24 -0500

    lol

---
+++ Instagram/PhotosViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Myles Johnson on 2/4/16.
+//  Copyright © 2016 Myles. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class PhotosViewController: UIViewController {
+    
+    var photos: [NSDictionary]?
+    
+    @IBOutlet weak var tableView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            self.photos = responseDictionary["data"] as? [NSDictionary]
+                            
+                    }
+                }
+        });
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
